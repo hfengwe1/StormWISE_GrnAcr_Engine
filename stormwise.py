ce3786e7010bd184c573d17660ca79f47626f4d7
Arthur E. McGarity <amcgarity@swarthmore.edu>
2016-07-27 12:29:12 -0400

    first commit separating model solution and results storage in dictionary from command line output

--- stormwise.py
+++ stormwise.py
@@ -8,7 +8,7 @@ and then runs AMPL on the dat file to generate stormwise output
 import yaml
 from subprocess import call
 from yaml_ampl import yaml_to_ampl
-def main():
+def stormwise():
     with open('wingohocking.yaml', 'r') as fin:
         doc = yaml.load(fin)
         ampl = yaml_to_ampl(doc)
@@ -17,7 +17,6 @@ def main():
         J = doc['J']
         K = doc['K']
         KONJ = doc['KONJ']
-        KONJ2 = doc['KONJ2']
         T = doc['T']
         convert = doc['convert']
     with open('wingohocking.dat', 'w') as fout:     
@@ -85,38 +84,119 @@ def main():
         print "Actual Total Investment Required:   $%10.2f Million" % investTotalMillions
         # Decision Space Output:
         print "\nDecision Space Output:"
+        zoneTot = {}
+        luTot = {}
+        giTot = {}
         for i in sorted(I):  # Compute and output total investments by zone
-            zoneTot = 0.0
+            zoneTot[i] = 0.0
             for j in sorted(J):
                 if KONJ[j] != None:
                     for k in sorted(KONJ[j]):
-                        zoneTot += x[i][j][k]
-            zoneTotMillions = zoneTot/1e6
+                        zoneTot[i] += x[i][j][k]
+            zoneTotMillions = zoneTot[i]/1e6
             print '\nZone:  %s with Total investment in zone =  %10.2f Million (details below)' % (i,zoneTotMillions)
             print '\n%18s' % '',
             for k in sorted(K):
                 print "%16s" % k,
             print "%18s" % 'Land Use Total'
-            #giLuTotalMillion = {}
+            luZoneTot = {}
             for j in sorted(J):  # compute and output total investments by landuse
-                #giLuTotalMillion[j] = 0.0
                 print "%-18s" % j,
-                giTot = 0.0
+                luZoneTot[j] = 0.0
                 for k in sorted(K):
                     if KONJ[j] != None:
                         if k in KONJ[j]:
-                            giTot += x[i][j][k]
+                            luZoneTot[j] += x[i][j][k]
                             investMillion = x[i][j][k]/1e6
-                            #giLuTotalMillion[j] += investMillion
                             print "%18.2f" % investMillion,
                         else:
                             print "%18.2f" % 0.0,
                     else:
                         print "%18.2f" % 0.0,
-                giTotMillion = giTot/1e6
-                print "%18.2f" % giTotMillion
+                luZoneTotMillion = luZoneTot[j]/1e6
+                print "%18.2f" % luZoneTotMillion
                 print ''
+                luTot[i] = luZoneTot
+            giZoneTot = {}
+            for k in sorted(K):
+                giZoneTot[k] = 0.0
+                for j in sorted(J):
+                    if KONJ[j] != None:
+                        if k in KONJ[j]:
+                            giZoneTot[k] += x[i][j][k]
+            print "%18s" % "GI Practice Total",
+            giTot[i] = giZoneTot
+            for k in sorted(K):
+                giTotMillion = giZoneTot[k]/1e6
+                print "%18.2f" % giTotMillion,
+    #print ''
+    #print zoneTot
+    #print luTot
+    #print giTot
+    return ()
+
+def storm_max(inYamlFile):
+    with open(inYamlFile, 'r') as fin:
+        doc = yaml.load(fin)
+        #ampl = yaml_to_ampl(doc)
+        # store the structure of the problem as found in the YAML file
+        I = doc['I']
+        J = doc['J']
+        K = doc['K']
+        KONJ = doc['KONJ']
+        T = doc['T']
+        convert = doc['convert']
+        cost = doc['cost']
+        export = doc['export']
+        area = doc['area']
+        eta = doc['eta']
+        f = doc['f']
+        
+        
+        
+param s{i in I,j in J,k in KONJ[j],t in T} = eta[i,k,t]*export[j,t]/cost[j,k];	# calculated benefit slopes
+param u{i in I,j in J,k in KONJ[j]} = cost[j,k]*f[i,j,k]*area[i,j];				# calculated upper spending limits
+
+        
+    #with open('wingohocking.dat', 'w') as fout:     
+    #    fout.write(ampl)
+    #    fout.close()
+    #call(["/Applications/amplide.macosx64/ampl","min_cost.run"])
+    #with open('min_cost.yaml', 'r') as fin:
+    #    solution = yaml.load(fin)
+    #    x = solution['x']
+    #    u = solution['u']
+     #   s = solution['s']
+        
+        # PRINT OUT RESULTS TO CONSOLE:
+        benefitUnits = {'1_volume': 'Million Gallons', '2_sediment': 'Tons',
+                        '3_nitrogen': 'Pounds', '4_phosphorous': 'Pounds'}        
+        print ""
+        maxBenefitTotal = {}
+        for t in T:
+            maxtot = 0.0
+            for i in sorted(I):
+                for j in sorted(J):
+                    if KONJ[j] != None:
+                        for k in sorted(KONJ[j]):
+                            spend = u[i][j][k]
+                            maxtot += convert[t]*s[i][j][k][t]*spend
+            maxBenefitTotal[t] = maxtot
+        print "Maximum Benefit Totals:"      
+        for t in sorted(maxBenefitTotal):
+            print "    %s:  %10.4f %s" % (t,maxBenefitTotal[t],benefitUnits[t])
+
+        maxInvestTotal = 0.0
+        for i in sorted(I):
+            for j in sorted(J):
+                if KONJ[j] != None:
+                    for k in sorted(KONJ[j]):
+                        spend = u[i][j][k]
+                        maxInvestTotal += spend
+        maxInvestMillions = maxInvestTotal/1e6
+        print "Maximum Total Investment Required:   $%10.2f Million" % maxInvestMillions
 
             
-main()
+#stormwise()
+storm_max('wingohocking.yaml')
 
